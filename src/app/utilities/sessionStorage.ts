3a54cd00527d5581ab101d2ed53d60c250f5dd75
Matías Gonta <maret2023@gmail.com>
2023-12-17 18:06:42 -0300

    Add session storage utilities

---
+++ src/app/utilities/sessionStorage.ts
@@ -0,0 +1,7 @@
+export const setSessionStorage = (key: string, value: any) => {
+    sessionStorage.setItem(key, JSON.stringify(value))
+}
+
+export const getSessionStorage = (key: string) => {
+    return sessionStorage.getItem(key)
+}
