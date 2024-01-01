70bae053860351da0d08521430899b1afb4098bf
Matías Gonta <maret2023@gmail.com>
2024-01-01 16:44:55 -0300

    Replace readme image :memo:

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const withNextIntl = require('next-intl/plugin')(
   './src/i18n.ts',
 );
 
-const nextConfig = {
-  images: {
-    domains: ['flagicons.lipis.dev'],
-  },
-}
+const nextConfig = {}
 
 module.exports = withNextIntl(nextConfig);
